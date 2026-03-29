b964f314da2ca535ce0fed91a7520ab7d3f0d007
kevin-fu1 <kevin_fu@outlook.com>
2026-03-29 20:29:32 +1300

    Fix Order book graph

--- src/pages/visualizer/OrdersChart.tsx
+++ src/pages/visualizer/OrdersChart.tsx
@@ -223,7 +223,7 @@ export function OrdersChart({ symbol }: OrdersChartProps): ReactNode {
       name: 'Other trades',
       color: '#a855f7',
       data: otherTradeData,
-      marker: { symbol: 'diamond', radius: 4 },
+      marker: { symbol: 'diamond', radius: 6 },
       tooltip: otherTradeTooltip,
       dataGrouping: { enabled: false },
     },
